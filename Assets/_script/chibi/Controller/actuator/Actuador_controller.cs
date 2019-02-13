a662d36091728a946203310185b2f5b7c5e3fa58
dem4ply <dem4ply@gmail.com>
2019-02-13 00:10:14 -0600

    preparacion de los actuadores

---
+++ Assets/_script/chibi/Controller/actuator/Actuador_controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace chibi.controller.actuator
+{
+	public class Actuador_controller : chibi.Chibi_behaviour
+	{
+		public chibi.controller.Controller controller;
+		public chibi.actuator.Actuator current_actuator;
+
+		public void action()
+		{
+			if ( current_actuator )
+				current_actuator.action( controller );
+			else
+				Debug.LogError(
+					string.Format( "no hay un actuador en {0}",
+					helper.game_object.name.full( this ) ) );
+		}
+	}
+}
---
+++ Assets/_script/chibi/actuator/Teleporter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace chibi.actuator
+{
+	public class Teleporter : Actuator
+	{
+
+		public override void action( controller.Controller controller )
+		{
+			Debug.Log(
+				string.Format( "[Actuador] {0} teletransportando a {1}",
+				helper.game_object.name.full( this ),
+				helper.game_object.name.full( controller ) ) );
+		}
+	}
+}
