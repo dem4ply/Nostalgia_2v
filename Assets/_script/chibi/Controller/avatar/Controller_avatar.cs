167465352c04d2db6c47d1a248443d1cb62412f6
dem4ply <dem4ply@gmail.com>
2019-03-10 15:39:39 -0600

    update general

---
+++ Assets/_script/chibi/Controller/avatar/Controller_avatar.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using chibi.motor;
+
+namespace chibi.controller.npc
+{
+	public class Controller_avatar : Controller
+	{
+		#region propiedades publicas
+		#endregion
+
+		protected override void _init_cache()
+		{
+			base._init_cache();
+		}
+	}
+}
