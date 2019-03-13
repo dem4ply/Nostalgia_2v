6d65f73a3f8ed8fed071c5f8218e7233af4dfd8a
dem4ply <dem4ply@gmail.com>
2019-03-12 20:55:42 -0600

    preparacion de los items

---
+++ Assets/_script/chibi/inventary/Item.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+
+namespace chibi.inventary
+{
+	public class Item: chibi.Chibi_behaviour
+	{
+		public item.Item item;
+		public int amount = 1;
+
+		public void send_to_pool()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void use( chibi.rol_sheet.Rol_sheet character )
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
+
---
+++ Assets/_script/chibi/inventary/item/Item.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+
+namespace chibi.inventary.item
+{
+	[ CreateAssetMenu( menuName="chibi/inventary/item/base" ) ]
+	public class Item: chibi.Chibi_object
+	{
+		public Sprite image;
+		public string description;
+		public chibi.inventary.Item prefab;
+		public int max_stack_amount;
+	}
+}
