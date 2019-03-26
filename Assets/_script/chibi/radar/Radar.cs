08377ed29ee1fba70f42851fe1fb7b7553daf5de
Dem4ply <dem4ply@users.noreply.github.com>
2019-03-25 22:31:56 -0600

    clases de radar

---
+++ Assets/_script/chibi/radar/Radar.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+namespace chibi.radar
+{
+	public class Radar
+	{
+		public Vector3 size;
+		public Vector3 direction;
+		public float distance;
+		public Quaternion rotation;
+
+		public List< LayerMask > masks;
+
+		public Transform origin;
+
+		public Dictionary< LayerMask, List< Radar_hit > > masks_hits;
+		public List< Radar_hit > hits;
+
+		public Radar(
+			Transform origin, Vector3 size, Vector3 direction, float distance,
+			Quaternion rotation, List<LayerMask> masks )
+		{
+			this.origin = origin;
+			this.size = size;
+			this.direction = direction;
+			this.distance = distance;
+			this.rotation = rotation;
+			this.masks = masks;
+
+			masks_hits = new Dictionary<LayerMask, List<Radar_hit>>();
+			hits = new List< Radar_hit >();
+		}
+
+		public virtual void ping()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Assets/_script/chibi/radar/Radar_box.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+namespace chibi.radar
+{
+	public class Radar_box : Radar
+	{
+		public Radar_box(
+			Transform origin, Vector3 size, Vector3 direction, float distance,
+			Quaternion rotation, List<LayerMask> masks )
+			: base( origin, size, direction, distance, rotation, masks )
+		{
+		}
+
+		public override void ping()
+		{
+			foreach ( LayerMask mask in masks )
+			{
+				RaycastHit[] current_hits = Physics.BoxCastAll(
+					origin.position, size, direction, rotation, distance, mask.value );
+
+				List<Radar_hit> results;
+				if ( masks_hits.TryGetValue( mask, out results ) )
+				{
+					results.Clear();
+					results.Capacity = current_hits.Length;
+				}
+				else
+				{
+					results = new List<Radar_hit>( current_hits.Length );
+					masks_hits.Add( mask, results );
+				}
+
+				for ( int i = 0; i < current_hits.Length; ++i )
+				{
+					if ( current_hits[ i ].transform != origin )
+					{
+						Radar_hit current_radar_hit =
+							new Radar_hit( current_hits[i] );
+						results.Add( current_radar_hit );
+						hits.Add( current_radar_hit );
+					}
+				}
+
+				if ( results.Count == 0 )
+					masks_hits.Remove( mask );
+			}
+		}
+	}
+}
