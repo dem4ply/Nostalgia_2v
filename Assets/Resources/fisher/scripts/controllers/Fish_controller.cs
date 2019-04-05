404c65d2e2c354caffd8e1a8893c2346f19941ea
Dem4ply <dem4ply@users.noreply.github.com>
2019-04-04 19:48:44 -0600

    spawner

---
+++ Assets/Resources/fisher/scripts/controllers/Fish_controller.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace fisher.controller
+{
+	public class Fish_controller : chibi.controller.Controller
+	{
+		public override void action( string name, string e )
+		{
+			base.action( name, e );
+		}
+	}
+}
